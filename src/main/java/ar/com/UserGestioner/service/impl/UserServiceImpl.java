3895682cdc6190e6c8a35ae7863c50b07c0d8104
jonathan.medina <jonathan.medina@globant.com>
2023-04-18 22:42:15 -0300

    feat agrego builder en la capa de servicios

--- src/main/java/ar/com/UserGestioner/service/impl/UserServiceImpl.java
+++ src/main/java/ar/com/UserGestioner/service/impl/UserServiceImpl.java
@@ -1,12 +1,12 @@
 package ar.com.UserGestioner.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ar.com.UserGestioner.model.entity.Phone;
+import ar.com.UserGestioner.builder.PhoneBuilder;
+import ar.com.UserGestioner.builder.UserBuilder;
 import ar.com.UserGestioner.model.entity.User;
 import ar.com.UserGestioner.model.request.UserRequest;
 import ar.com.UserGestioner.repository.UserRepository;
@@ -14,23 +14,24 @@ import ar.com.UserGestioner.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	UserRepository userRepository;
 
 	@Override
 	public void singUp(UserRequest userRequest) {
-		User user = new User();
-		user.setNombre("sdd");
-		Phone phone = new Phone();
-		phone.setNumber(123333);
-		phone.setCityCode(117);
-		List<Phone> phns= new ArrayList<>();
-		phns.add(phone);
-		user.setPhones(phns);
-		persist(user);
-
+		persist(buildUser(userRequest));
 	}
+
+	public User buildUser(UserRequest userRequest) {
+		return new UserBuilder().widthName(userRequest.getName()).widthPassword(userRequest.getPassword())
+				.widthPhones(userRequest.getPhones().stream()
+						.map((phone) -> new PhoneBuilder().widthNumber(phone.getNumber())
+								.widthCityCode(phone.getCityCode()).widthContryCode(phone.getContryCode()).build())
+						.collect(Collectors.toList()))
+				.widthEmail(userRequest.getEmail()).build();
+	}
+
 	public void persist(User user) {
 		userRepository.save(user);
 		userRepository.flush();
